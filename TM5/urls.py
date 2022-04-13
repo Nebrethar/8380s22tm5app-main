f0f6bdb13c99f4d8773fce4de45449401dff610b
Matt Snell <msnell@unomaha.edu>
2022-04-13 12:08:05 -0500

    Revert "First steps of dual serve"
    This reverts commit 3b8f5d06b76664b3f5982b9f181859d8ccbb49af.

--- TM5/urls.py
+++ TM5/urls.py
@@ -1,16 +1,15 @@
 from django.urls import include, path, re_path
 from rest_framework import routers
-from django.views.generic import TemplateView
 from music import views
-from django.urls import re_path
-from django.conf.urls.static import static
-from pathlib import Path
-import os
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    re_path(r'^$', TemplateView.as_view(template_name='index.html')),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('facebook-post/', views.post_facebook),
     path('twitter-post/', views.post_twitter),
