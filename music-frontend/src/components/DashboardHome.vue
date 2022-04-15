8bec34a904626a5bea0f371560f1b03cfedc7582
pjunius416 <52839119+pjunius416@users.noreply.github.com>
2022-04-14 23:41:48 -0500

    Cleaning Up Dashboard (Not Logged In)

--- music-frontend/src/components/DashboardHome.vue
+++ music-frontend/src/components/DashboardHome.vue
@@ -5,18 +5,28 @@
         
         <!-- Welcome Features -->
         <div class="row align-items-center justify-content-center">
-            <div class="card" style="width:1000px">
-                <img :src="require('@/assets/sun-icon.png')" class="card-img-top" style="height: 10rem; width: 10rem;">
-                <img :src="require('@/assets/music-icon.png')" class="float-right" style="height: 10rem; width: 10rem;"> 
-                    Now Playing: Enemy x Imagine Dragons
-                <h5>75&#176;</h5>
-                <div class="card-body">
-                    The weather can help to recommend some music.
-                    Login to use this feature for free!
+            <div class="card-group" style="width:900px">
+                <div class="card" >
+                    <img :src="require('@/assets/sun-icon.png')" class="mx-auto d-block" style="height: 15rem; width: 15rem;">   
+                    <div class="card-body">
+                        <h2 class="card-title">75&#176; Omaha, NE</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <img :src="require('@/assets/music-icon.png')" class="mx-auto d-block" style="height: 15rem; width: 15rem;"> 
+                    <div class="card-body">
+                        <h2 class="card-title">Now Playing: Enemy x Imagine Dragons</h2>
+                        The weather can help to recommend some music.
+                        Login to use this feature for free!
+                    </div>
                 </div>
             </div>
+        </div>
+        <br>
+        <div class="row align-items-center justify-content-center">
             <div class="card" style="width:1000px">
-                <iframe width="999" height="500" src="https://www.youtube.com/embed/D9G1VOjN_84" title="YouTube video player" 
+                <h2 class="card-title">Random Music Video: Enemy x Imagine Dragons</h2>
+                <iframe width="975" height="500" src="https://www.youtube.com/embed/D9G1VOjN_84" title="YouTube video player" 
                 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
