ab5ef6c878b6ef7f6b96d4d5e4d10027885a7882
pjunius416 <52839119+pjunius416@users.noreply.github.com>
2022-04-14 22:19:49 -0500

    Rough Draft of Dashboard (No Login)

--- music-frontend/src/components/DashboardHome.vue
+++ music-frontend/src/components/DashboardHome.vue
@@ -1,15 +1,24 @@
 <template>
-    <!-- Welcome Text -->
-    <h1><b>Welcome to the Music Recommendation App!</b></h1>
-
-    <!-- Welcome Features -->
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">The weather can help to recommend some music.</p>
-            <p class="card-text">Login to use this feature for free!.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+    <div>
+        <!-- Welcome Text -->
+        <h1><b>Welcome to the Music Recommendation App!</b></h1>
+        
+        <!-- Welcome Features -->
+        <div class="row align-items-center justify-content-center">
+            <div class="card" style="width:1000px">
+                <img :src="require('@/assets/sun-icon.png')" class="card-img-top" style="height: 10rem; width: 10rem;">
+                <img :src="require('@/assets/music-icon.png')" class="float-right" style="height: 10rem; width: 10rem;"> 
+                    Now Playing: Enemy x Imagine Dragons
+                <h5>75&#176;</h5>
+                <div class="card-body">
+                    The weather can help to recommend some music.
+                    Login to use this feature for free!
+                </div>
+            </div>
+            <div class="card" style="width:1000px">
+                <iframe width="999" height="500" src="https://www.youtube.com/embed/D9G1VOjN_84" title="YouTube video player" 
+                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
         </div>
     </div>
 </template>
