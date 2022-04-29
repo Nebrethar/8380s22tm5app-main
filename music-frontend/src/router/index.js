32a9ccd27d951c50586b23e591c640e38cc18844
Matt Snell <msnell@unomaha.edu>
2022-04-29 08:23:59 -0500

    Fix some social media bugs

--- music-frontend/src/router/index.js
+++ music-frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import DashboardHome from '@/components/DashboardHome.vue'
 import Base from '@/components/Base.vue'
 import NotFoundComponent from '@/components/NotFoundComponent.vue'
@@ -91,7 +91,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  mode: 'history',
+  history: createWebHistory(),
   routes
 })
 
