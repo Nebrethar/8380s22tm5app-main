1cae4316d95f5351d55bc105561098fa91858ff2
pjunius416 <52839119+pjunius416@users.noreply.github.com>
2022-05-03 22:51:27 -0500

    Starting Point for History Page

--- music-frontend/src/components/History.vue
+++ music-frontend/src/components/History.vue
@@ -4,6 +4,24 @@
     <div  class="text-white">
         <!-- Welcome Text -->
         <h1 class="py-sm-5 masthead-heading mb-0" ><b>History</b></h1>
+        <table class="table table-sm">
+            <tr>
+              <th scope="col">Song</th>
+              <th scope="col">Artist</th>
+              <th scope="col">Found By</th>
+              <th scope="col">Spotify Link</th>
+              <th scope="col">Youtube Link</th>
+            </tr>
+            <tbody>
+            <tr v-for = "song in songs" v-bind:key="song">
+              <th scope="row">{{song.song}}</th>
+              <td>{{song.artist}}</td>
+              <td>{{song.source}}</td>
+              <td>{{song.sf_link}}</td>
+              <td>{{song.yt_link}}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -15,12 +33,33 @@ const apiService = new APIService();
 
 export default {
   name: 'History',
+
+  data() {
+    return {
+      songs: []
+    }
+  },
+  methods: {
+    getHistory() {
+      apiService.getPlaylists().then(response => {
+        this.songs = response.data;
+      }).catch(error => {
+        if (error.response.status === 401) {
+          localStorage.removeItem('isAuthenticates');
+          localStorage.removeItem('log_user');
+          localStorage.removeItem('token');
+          router.push("/auth");
+        }
+      });
+    }
+  },
   mounted: function () {
     history.pushState(
         {},
         null,
         '/'
-    )
+    ),
+    this.getHistory();
   }
 }
 </script>
